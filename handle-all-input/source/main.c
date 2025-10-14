af0ff9f9a90549085ad15a0a10add9546e73115c
Tay <tay@xenon>
2025-10-14 22:31:29 +0000

    Input handling boilerplate

---
+++ handle-all-input/source/main.c
@@ -0,0 +1,73 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <console/console.h>
+#include <console/telnet_console.h>
+#include <xenos/xenos.h>
+#include <input/input.h>
+#include <network/network.h>
+#include <xenon_uart/xenon_uart.h>
+#include <usb/usbmain.h>
+#include <xenon_smc/xenon_smc.h>
+
+int main(){
+	xenos_init(VIDEO_MODE_AUTO);
+	console_init();
+	telnet_console_init();
+	
+	// Initialize 360 controller - taken from XeLL kbootconf.c
+	struct controller_data_s ctrl;
+
+	printf("On controller, telnet, or UART: Press X to reload XeLL. Y to shutdown. B to reboot.");
+
+	// Controller, UART, or telnet can be used now to control things.
+	while(1){
+		// For controllers
+		usb_do_poll();
+
+		// For telnet
+		network_poll();
+		
+		// Controller 
+		if (get_controller_data(&ctrl, 0)) {
+			if (ctrl.x){
+				exit(0);
+				for(;;);
+				break;
+			} if (ctrl.y){
+				xenon_smc_power_shutdown();
+				for(;;);
+				break;
+			} if (ctrl.b){
+				xenon_smc_power_reboot();
+				for(;;);
+				break;
+			}
+		}
+
+		// If either telnet or UART send a keystroke, see what char it was and process it.
+		if(kbhit()){
+			switch(getch()){
+				case 'x':
+					// Try reloading XeLL from NAND. (1f, 2f, gggggg)
+					// Platform specific functioanlity defined in libxenon/drivers/newlib/xenon_syscalls.c
+					exit(0);
+					break;
+				case 'h':
+					xenon_smc_power_shutdown();
+					for(;;);
+					break;
+				case 'r':
+					xenon_smc_power_reboot();
+					for(;;);
+					break;
+				default:
+					printf("Detected char.\n");
+			}
+		}
+	}
+
+	// Return to XeLL on NAND or else hard reboot.
+	exit(0);
+}
